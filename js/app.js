d0c11ac1b1ed82cfa6668d0d09e2bb83ce014959
Computer <Computer>
2017-08-23 03:39:15 +0200

    beatify js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function(x,y,speed) {
+var Enemy = function(x, y, speed) {
 
     this.sprite = 'images/enemy-bug.png';
     //Setting the Enemy initial location
-    this.x =  (Math.random() * 60 ) ;
-    this.y =Math.floor(Math.random() * (230 - 60 + 1)) + 60;
+    this.x = (Math.random() * 60);
+    this.y = Math.floor(Math.random() * (230 - 60 + 1)) + 60;
     this.speed = 40;
 
-    
+
 
 };
 
@@ -19,8 +19,7 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.dt = 20;
     this.x = this.x + this.speed * dt;
-    if (this.x > 500)
-    {
+    if (this.x > 500) {
         this.x = Math.floor(Math.random() * (-1 + 7 + 1)) - 7;
     }
     // console.log(this.x);
@@ -32,7 +31,7 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
-var Player = function(x,y,speed) {
+var Player = function(x, y, speed) {
 
     this.sprite = 'images/char-boy.png';
     this.x = 202;
@@ -43,50 +42,49 @@ var Player = function(x,y,speed) {
 // This class requires an update(), render() and
 // a handleInput() method.
 Player.prototype.update = function(dt) {
-     if(this.y <= -15)
-     {
+    if (this.y <= -15) {
         this.reset();
-     }
+    }
 };
-Player.prototype.render = function () {
+Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
 };
 
-Player.prototype.handleInput = function (keypress) {
+Player.prototype.handleInput = function(keypress) {
 
-  if(keypress === 'left' && this.x > 40){
-     this.x -= 100;
- }
- if(keypress === 'up' && this.y > 0){
- this.y -= 83;
+    if (keypress === 'left' && this.x > 40) {
+        this.x -= 100;
+    }
+    if (keypress === 'up' && this.y > 0) {
+        this.y -= 83;
 
- }
- if(keypress === 'right' && this.x < 400){
- this.x += 100;
+    }
+    if (keypress === 'right' && this.x < 400) {
+        this.x += 100;
 
- }
- if(keypress === 'down' && this.y < 400){ 
- this.y += 83;
+    }
+    if (keypress === 'down' && this.y < 400) {
+        this.y += 83;
 
- }
+    }
 
 };
-Player.prototype.checkCollisions = function(){
-  for (var i = 0; i < allEnemies.length; i++){
-     if ((Math.abs(this.x - allEnemies[i].x)) < 101 && (Math.abs(this.y - allEnemies[i].y)) < 50){
-        console.log(this.y);
-        this.reset();
-     }
-  }
+Player.prototype.checkCollisions = function() {
+    for (var i = 0; i < allEnemies.length; i++) {
+        if ((Math.abs(this.x - allEnemies[i].x)) < 101 && (Math.abs(this.y - allEnemies[i].y)) < 50) {
+            console.log(this.y);
+            this.reset();
+        }
+    }
 };
 //reset player location after collision
-Player.prototype.reset = function(){
+Player.prototype.reset = function() {
     this.x = 202;
     this.y = 400;
 };
 // Now instantiate your objects.
-var enemy1 = new Enemy(50,60,10);
+var enemy1 = new Enemy(50, 60, 10);
 // var enemy2 = new Enemy(100,145,40);
 // var enemy3 = new Enemy(130,220,50);
 
